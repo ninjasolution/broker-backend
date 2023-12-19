11b4f18ddecbe1a0815a7e9cbc771843a5df8b53
Ninja Solution <top1successfulman@gmail.com>
2023-12-19 13:31:53 +1000

    add second loan form

--- src/service/index.js
+++ src/service/index.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const { MailService } = require('@sendgrid/mail');
 const LoanRequestEmail = require('../email-templates/loanRequestEmail');
+const Loan2RequestEmail = require('../email-templates/loan2RequestEmail');
 const ContactRequestEmail = require('../email-templates/contactRequestEmail');
 
 class Service {
@@ -13,6 +14,30 @@ class Service {
 
     async loanRequest(data) {
         const html = LoanRequestEmail(data);
+
+        const from = `${process.env.SEMD_GRID_COMPANY_MAIL}`;
+        const to = `${process.env.RECIPIENT_MAIL}`;
+
+        //convert the email and password to base64 format
+        const message = {
+            from,
+            to,
+            subject: `New Loan Request`,
+            html,
+        };
+
+        const status = await this.mailService.send(message).then(res => {
+            return 'success';
+        }).catch(err => {
+            console.log('^-^Error : ', err);
+            return 'failed';
+        })
+
+        return status;
+    }
+
+    async loan2Request(data) {
+        const html = Loan2RequestEmail(data);
 
         const from = `${process.env.SEMD_GRID_COMPANY_MAIL}`;
         const to = `${process.env.RECIPIENT_MAIL}`;
